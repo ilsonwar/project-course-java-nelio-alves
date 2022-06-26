903fd55a3dea2c87535c919ff3dfd23b9a8d0ab8
Ilson Warmling <ilsonwarsc@gmail.com>
2022-06-26 20:31:22 -0300

    ForEach repetitive structure

---
+++ src/forEach/ForEach.java
@@ -0,0 +1,12 @@
+package forEach;
+
+public class ForEach {
+    public static void main(String[] args) {
+
+        String names[] = new String[]{"Ilson", "João", "Mary"};
+
+        for (String apelido : names) {
+            System.out.println(apelido);
+        }
+    }
+}
