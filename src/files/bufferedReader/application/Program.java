2feb5eb5bf293c09e8a391ac4396ed5c6b2666e8
Ilson Warmling <ilsonwarsc@gmail.com>
2022-07-26 22:18:04 -0300

    Improving the buffered

--- src/files/bufferedReader/application/Program.java
+++ src/files/bufferedReader/application/Program.java
@@ -7,11 +7,8 @@ import java.io.IOException;
 public class Program {
     public static void main(String[] args) {
         String path = "C:\\temp\\in.txt";
-        BufferedReader br = null;
-        FileReader fr = null;
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
             while (line != null) {
                 System.out.println(line);
@@ -19,15 +16,6 @@ public class Program {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null)
-                    br.close();
-                if (fr != null)
-                    fr.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
