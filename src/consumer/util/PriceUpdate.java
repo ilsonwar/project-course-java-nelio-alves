23e21aadbf28a41774d0df72e5780bb3ff7f1dd9
Ilson Warmling <ilsonwarsc@gmail.com>
2022-09-01 12:37:34 -0300

    Consumer

---
+++ src/consumer/util/PriceUpdate.java
@@ -0,0 +1,13 @@
+package consumer.util;
+
+import consumer.entities.Product;
+
+import java.util.function.Consumer;
+
+public class PriceUpdate implements Consumer<Product> {
+
+    @Override
+    public void accept(Product p) {
+        p.setPrice(p.getPrice() * 1.1);
+    }
+}
